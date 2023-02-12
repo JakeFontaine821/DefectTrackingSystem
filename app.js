f213d6d5ef4aeb01e140b994842f31140eab2737
Jake Fontaine <72557178+JakeFontaine821@users.noreply.github.com>
2023-02-12 02:35:50 -0500

    first completed build of dts

--- app.js
+++ app.js
@@ -23,52 +23,32 @@ var IssueLog = mongoose.model("issueLog");
 
 //example routes
 app.get("/", function(req, res){
-    res.redirect("index.html");
+    res.redirect("errorList.html");
 })
 
 app.post("/saveError", function(req, res){
-    console.log(req.body)
+    new IssueLog(req.body).save().then(function(){
+        res.redirect("errorList.html");
+    })
 })
 
+app.get("/errorLogs", function(req, res){
+    IssueLog.find({}).then(function(log){
+        res.json({log});
+    })
+})
 
+app.post("/deleteLog", function(req, res){
+    IssueLog.findByIdAndDelete(req.body.log).exec();
+    res.redirect("errorList.html");
+})
 
-// app.get("/", function(req, res){
-//     res.redirect("gameList.html");
-// })
-
-// app.post("/saveGame", function(req, res){
-//     console.log(req.body);
-
-//     new Game(req.body).save().then(function(){
-//         //res.send(req.body); 
-//         res.redirect("index.html");
-//     });
-// })
-
-// app.get("/getGames", function(req, res){
-//     Game.find({}).then(function(game){
-//        // console.log({game});
-//         res.json({game});
-//     })
-// })
-
-// app.post("/deleteGame", function(req, res){
-//     console.log(`Game Deleted ${req.body.game}`);
-//     Game.findByIdAndDelete(req.body.game).exec();
-//     res.redirect('gameList.html');
-// })
-
-// app.get("/getID::id", function(req, res){
-//     console.log(req.body.game);
-//     res.redirect("updatePage.html?id=" + req.params._id);
-// })
-
-// app.post("/updateGame", function(req, res){
-//     console.log(req.body);
-//     Game.findByIdAndUpdate(req.body.id, {game:req.body.game}, function(){
-//         res.redirect("gameList.html");
-//     })
-// })
+app.post("/updateLog", function(req, res){
+    console.log(req.body);
+    IssueLog.findByIdAndUpdate(req.body.id, {status:req.body.status}, function(){
+        res.redirect("errorList.html");
+    })
+})
 
 app.use(express.static(__dirname+"/pages"));
 app.listen(port, function(){
